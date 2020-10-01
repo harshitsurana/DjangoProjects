b7b6d3892860fa72fde81c075b32b0e029127484
harshitjsurana <55993308+harshitjsurana@users.noreply.github.com>
2020-10-01 14:11:48 +0530

    Update view.py
    Added LoginRequiredMixin so that any body else cannot delete/alter others data.
    And anyone publically cannot create new make and autos.

--- autos/views.py
+++ autos/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -8,7 +9,7 @@ from autos.forms import MakeForm
 
 # Create your views here.
 
-class MainView(View) :
+class MainView(LoginRequiredMixin, View) :
     def get(self, request):
         mc = Make.objects.all().count();
         al = Auto.objects.all();
@@ -16,7 +17,7 @@ class MainView(View) :
         ctx = { 'make_count': mc, 'auto_list': al };
         return render(request, 'autos/auto_list.html', ctx)
 
-class MakeView(View) :
+class MakeView(LoginRequiredMixin,View) :
     def get(self, request):
         ml = Make.objects.all();
         ctx = { 'make_list': ml };
@@ -24,7 +25,7 @@ class MakeView(View) :
 
 # We use reverse_lazy() because we are in "constructor attribute" code
 # that is run before urls.py is completely loaded
-class MakeCreate(View):
+class MakeCreate(LoginRequiredMixin, View):
     template = 'autos/make_form.html'
     success_url = reverse_lazy('autos:all')
     def get(self, request) :
@@ -44,7 +45,7 @@ class MakeCreate(View):
 # MakeUpdate has code to implement the get/post/validate/store flow
 # AutoUpdate (below) is doing the same thing with no code
 # and no form by extending UpdateView
-class MakeUpdate(View):
+class MakeUpdate(LoginRequiredMixin, View):
     model = Make
     success_url = reverse_lazy('autos:all')
     template = 'autos/make_form.html'
@@ -64,7 +65,7 @@ class MakeUpdate(View):
         form.save()
         return redirect(self.success_url)
 
-class MakeDelete(View):
+class MakeDelete(LoginRequiredMixin, View):
     model = Make
     success_url = reverse_lazy('autos:all')
     template = 'autos/make_confirm_delete.html'
@@ -84,17 +85,17 @@ class MakeDelete(View):
 # These views do not need a form because CreateView, etc.
 # Build a form object dynamically based on the fields
 # value in the constructor attributes
-class AutoCreate(CreateView):
+class AutoCreate(LoginRequiredMixin,CreateView):
     model = Auto
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
 
-class AutoUpdate(UpdateView):
+class AutoUpdate(LoginRequiredMixin, UpdateView):
     model = Auto
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
 
-class AutoDelete(, DeleteView):
+class AutoDelete(LoginRequiredMixin, DeleteView):
     model = Auto
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
